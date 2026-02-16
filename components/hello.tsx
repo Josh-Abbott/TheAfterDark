ed2f36c8182bb6cc82da5fe7ff755ac899d676bf
Josh Abbott <abbott.m.josh@gmail.com>
2026-02-16 04:38:23 -0800

    Create favicon, begin early structure setup

---
+++ components/hello.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react'
+
+const Hello = () => {
+    console.log('Client Component');
+    return (
+        <div>Welcome to the website!</div>
+    )
+}
+
+export default Hello
